f676a323bebf25172aeedfa1cf360f1f43675c12
efmmoncada <emmanuel.moncada27@gmail.com>
2023-02-23 11:18:52 -0800

    add http client packages
    Signed-off-by: efmmoncada <emmanuel.moncada27@gmail.com>

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import { React, useState } from "react";
 import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { NavigationContainer } from "@react-navigation/native";
 import TabsNavigator from "./navigation/TabsNavigator";
 import MainNavigator from "./navigation/MainNavigator";
 import TeamData from "./data/teamData.json";
 import * as team from "./data/teamData.json";
 
+const queryClient = new QueryClient();
+
 export default function App() {
   const [drillData, setDrillData] = useState(TeamData.drills);
-  return <MainNavigator />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MainNavigator />
+    </QueryClientProvider>
+  )
 }
 
 const styles = StyleSheet.create({
