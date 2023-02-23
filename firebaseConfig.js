4f8e65c33512eabbda1f4a1d2c778d96e65a9a96
efmmoncada <emmanuel.moncada27@gmail.com>
2023-02-22 23:10:39 -0800

    initialize firebase
    Signed-off-by: efmmoncada <emmanuel.moncada27@gmail.com>

---
+++ firebaseConfig.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAJAQMt-eOyg5PWPzk7kX7RaF9x6gnUEfo",
+  authDomain: "osu-golf-drill-challenge-app.firebaseapp.com",
+  projectId: "osu-golf-drill-challenge-app",
+  storageBucket: "osu-golf-drill-challenge-app.appspot.com",
+  messagingSenderId: "267068594844",
+  appId: "1:267068594844:web:eaed0d9810352c7295ab59",
+  measurementId: "G-QFKYHGXKNY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
