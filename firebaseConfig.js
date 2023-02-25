4e38db952631ea919ad45a148edcedbea08bc46e
efmmoncada <emmanuel.moncada27@gmail.com>
2023-02-24 21:42:59 -0800

    initialize firestore and make db available to project
    Signed-off-by: efmmoncada <emmanuel.moncada27@gmail.com>

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,3 +19,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+export { app, db };
