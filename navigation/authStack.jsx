5ccea970def2ffd467e64f3c255780743abf75e8
BrendanBaldocchi2 <91345107+BrendanBaldocchi2@users.noreply.github.com>
2023-03-10 16:13:17 -0800

    Added sign up and sign in functionality. Changed firebase config and added useAuth, appStack, authStack

---
+++ navigation/authStack.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import SignIn from "../screens/SignIn";
+import Player from "../screens/Player";
+import Coach from "../screens/Coach";
+import SignUp from "../screens/SignUp";
+import TabsNavigator from "./TabsNavigator";
+
+const AuthStack = createStackNavigator();
+
+export default function Navigator(){
+    return(
+    <NavigationContainer>
+        <AuthStack.Navigator screenOptions={{ headerShown: false }}>
+            <AuthStack.Screen name="SignIn" component={SignIn} />
+            <AuthStack.Screen name="SignUp" component={SignUp} />
+            <AuthStack.Screen name="Player" component={Player} />
+            <AuthStack.Screen name="Coach" component={Coach} />
+
+        </AuthStack.Navigator>
+    </NavigationContainer>
+    )
+}
